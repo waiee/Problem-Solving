e6e303c45826608401a996759487b95f00363bc9
waiee <waiee_z@yahoo.com>
2022-11-29 15:59:00 +0800

    setup dict

--- Python/algo.py
+++ Python/algo.py
@@ -38,10 +38,19 @@ total_ta = 0
 
 for i in range(np):
     process_info = {}
-    process_info['id'] = str(i+0)
-    process_info['arrival_time'] = int(input("Enter arrival time for P" + str(i+0) + ": "))
-    
-print(process_info)
+    process_info['id'] = str(i+1)
+    process_info['arrival_time'] = int(input("Enter arrival time for P" + str(i+1) + ": "))
+    process_info['burst_time'] = int(input("Enter burst time for P" + str(i+1) + ": "))
+
+    info.append(process_info)
+
+print("{:<15} {:<15} {:<15}".format('Process','Arrival Time','Burst Time'))
+
+for i in range(np):
+
+    p=info[i]
+
+    print("{:<15} {:<15} {:<15}".format(p['id'],p['arrival_time'],p['burst_time']))
 
 
 
