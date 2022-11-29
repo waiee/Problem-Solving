4acf16f426d4569ae349cf125f81bcfb5d88e14f
waiee <waiee_z@yahoo.com>
2022-11-29 23:57:57 +0800

    commit

--- Python/algo.py
+++ Python/algo.py
@@ -1,16 +1,16 @@
 import numpy as np
 
 # User insert number of process
-np = int(input("Enter number of processes: "))
+n = int(input("Enter number of processes: "))
 print("")
 
 info = []
 total_wt = 0
 total_ta = 0
 
-for i in range(np):
+for i in range(n):
     process_info = {}
-    print("(P" + str(i+1) + ")")
+    print("[P" + str(i+1) + "]")
     process_info['id'] = str(i+1)
     process_info['arrival_time'] = int(
         input("Enter Arrival Time: "))
@@ -28,7 +28,7 @@ info.sort(key=sort_param)
 # Menu
 print("{:<15} {:<15} {:<15}".format('Process', 'Arrival Time', 'Burst Time'))
 
-for i in range(np):
+for i in range(n):
     p = info[i]
     print("{:<15} {:<15} {:<15}".format(
         p['id'], p['arrival_time'], p['burst_time']))
