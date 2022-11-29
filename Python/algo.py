d5f5e59a81cd3688c572bf180a3b1a7fc67a9cd7
waiee <waiee_z@yahoo.com>
2022-11-29 15:44:35 +0800

    restart

--- Python/algo.py
+++ Python/algo.py
@@ -1,31 +1,32 @@
-import numpy as np
+# import numpy as np
 
-#Enter number or processes
-np = int(input("Enter number of processes: "))
+# #Enter number or processeswhile True:
+# np = int(input("Enter number of processes: "))
 
 #loop for arrival time, burst time
-position = []
-arrival_time = []  
-burst_time = []
+# position = []
+# arrival_time = []  
+# burst_time = []
 
-def sorted(arr):
-    arrival_time.sort()
-    return arr
+# def sorted(arr):
+#     arrival_time.sort()
+#     return arr
 
-for i in range(np):
-    position.append(i+1)
-    arrival_time.append(int(input("Enter arrival time for P" + str(i+1) + ": ")))
-    burst_time.append(int(input("Enter burst time for P" + str(i+1) + ": ")))
+# for i in range(np):
+#     position.append(i+1)
+#     arrival_time.append(int(input("Enter arrival time for P" + str(i+1) + ": ")))
+#     burst_time.append(int(input("Enter burst time for P" + str(i+1) + ": ")))
 
-sorted(arrival_time)
-# print(arrival_time)
+# sorted(arrival_time)
+# # print(arrival_time)
 
-#menu
-print("---------------------------------")
-print("Process   Arrival Time   Burst Time")
-for i in range(len(position)):
-    print("P" + str(position[i]) + "        " + str(arrival_time[i]) +
-     "\t\t " + str(burst_time[i]))
+# #menu
+# print("---------------------------------")
+# print("Process   Arrival Time   Burst Time")
+# for i in range(len(position)):
+#     print("P" + str(position[i]) + "        " + str(arrival_time[i]) +
+#      "\t\t " + str(burst_time[i]))
+
 
 
 
