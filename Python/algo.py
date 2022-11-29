f9184adf739b95d15b678e07b53430767b1e58fa
waiee <waiee_z@yahoo.com>
2022-11-29 13:17:31 +0800

    array for arrival time

--- Python/algo.py
+++ Python/algo.py
@@ -4,3 +4,8 @@
 np = int(input("Enter number of processes: "))
 
 #loop for arrival time, burst time
+arrival_time = []
+burst_time = []
+
+for i in range(np):
+    arrival_time.append(int(input("Enter arrival time for P" + i)))
