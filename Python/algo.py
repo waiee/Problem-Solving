dfa8821f8ebdcfbd347ac8bdb55e7e73f4996498
waiee <waiee_z@yahoo.com>
2022-11-29 13:27:21 +0800

    setup menu

--- Python/algo.py
+++ Python/algo.py
@@ -4,8 +4,18 @@
 np = int(input("Enter number of processes: "))
 
 #loop for arrival time, burst time
+position = []
 arrival_time = []
 burst_time = []
 
 for i in range(np):
-    arrival_time.append(int(input("Enter arrival time for P" + i)))
+    position.append(i+1)
+    arrival_time.append(int(input("Enter arrival time for P" + str(i+1) + ": ")))
+
+print("---------------------------------")
+print("Process   Arrival time")
+print("P" + str(position[0]) + "        " + str(arrival_time[0]))
+
+
+
+
