d2293d2c47f5407f01d094cfcbdcce0bcb5bd453
waiee <waiee_z@yahoo.com>
2022-11-30 11:32:36 +0800

    waiting time

--- Python/algo.py
+++ Python/algo.py
@@ -22,8 +22,12 @@ for i in range(n):
     print("")
 
 # Sort by arrival time
+
+
 def sort_param(e):
     return e['arrival_time']
+
+
 info.sort(key=sort_param)
 
 # Menu
@@ -35,21 +39,28 @@ for i in range(n):
         p['id'], p['burst_time'], p['arrival_time']))
 
 
-
 # Algorithm
-# time = 0
-# total_time = 0
-# bt = 0
+time = 0
+total_time = 0
+bt = 0
+wt = 0
 
-# for i in range(np):
-#     process_info = info[i]
+for i in range(n):
+    process_info = info[i]
+    bt = process_info['burst_time']
+    wt = process_info['waiting_time']
 
-#     if i==0:
-#         # process_info['wait_time'] = 0
-#         bt = process_info['burst_time']
-#         time+= bt
+    # if i == 0:
+    #     for i in range(bt+1):
+    #         if i == 0:
+    #             process_info['wait_time'] = 0
+    #             wt = process_info['wait_time']
+    #             time += bt
+    #             print("Waiting Time: " + str(wt))
+    #             print(str(time) + " second executed.")
 
-#     else: 
-
+    #         else:
+    #             print("this is 1 second afterward")
+    if i == 0:
 
 
