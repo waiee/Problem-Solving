b58b29a0d97f777e4164446ee32b972028b96875
waiee <waiee_z@yahoo.com>
2022-12-06 00:17:11 +0800

    FUCKKK I GIVE UP

--- Python/algo.py
+++ Python/algo.py
@@ -94,7 +94,7 @@ for i in range(size):
     if i == 0:
     #check if arrival time sama, tapi diff burst time
        for j in range(size):
-        if j != size-1:
+        if j != size:
             copy_pi = info[j+1]
             at_copy = copy_pi['arrival_time']
             bt_copy = copy_pi['burst_time']
