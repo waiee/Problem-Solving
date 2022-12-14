61b72b73a6f0dd37783585c745a912ba4d907fe1
waiee <waiee_z@yahoo.com>
2022-12-14 15:27:17 +0800

    main main

--- Java/Vehicle.java
+++ Java/Vehicle.java
@@ -1,9 +1,23 @@
-public class Vehicle {
+public abstract class Vehicle {
+
+    private String carName;
+    private String carBrand;
     
+    public Vehicle(){
+    };
+
+    public Vehicle(String cn, String cb){
+        this.carName = cn;
+        this.carBrand = cb;
+    }
+
+    public String getName{
+
+    }
 }
 
 class testVehicle {
     public static void main(String args[]){
-        
+
     }
 }
