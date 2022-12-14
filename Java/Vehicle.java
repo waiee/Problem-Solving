6d46e2bfec54e70b8cc93bbac0fb499c0e8196a0
waiee <waiee_z@yahoo.com>
2022-12-14 15:30:51 +0800

    hm

--- Java/Vehicle.java
+++ Java/Vehicle.java
@@ -1,27 +1,9 @@
-public abstract class Vehicle {
-
-    private String carName;
-    private String carBrand;
-    
-    public Vehicle(){
-    };
-
-    public Vehicle(String cn, String cb){
-        this.carName = cn;
-        this.carBrand = cb;
-    }
-
-    public String getName(){
-        return carName;
-    }
-
-    public String getBrand(){
-        return carBrand;
-    }
+public class Vehicle {
 }
 
-class testVehicle {
-    public static void main(String args[]){
+class Bus extends Vehicle {
+}
 
-    }
+class Taxi extends Vehicle{
+    
 }
