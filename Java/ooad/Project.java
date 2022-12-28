818f7158c5a31ee2351814ce45336e85c65cb9b4
waiee <waiee_z@yahoo.com>
2022-12-28 15:49:19 +0800

    commit

--- Java/ooad/Project.java
+++ Java/ooad/Project.java
@@ -1,7 +1,37 @@
 package ooad;
 
+import java.util.ArrayList;
+
 public class Project {
     private String projectName;
-    private String status;
-    private String comment;
-}
+    private boolean status;
+    private ArrayList<String> comment;
+    
+  
+    public Project(String projectName, boolean status) {
+      this.projectName = projectName;
+      this.status = status;
+    }
+  
+    public String getProjectName() {
+      return projectName;
+    }
+  
+    public boolean getStatus() {
+      return status;
+    }
+  
+    public void setProjectName(String projectName) {
+      this.projectName = projectName;
+    }
+  
+    public void setStatus(boolean status) {
+      this.status = status;
+    }
+  }
+  
+  class testProject{
+    Project project = new Project("Project 1", true);
+    System.out.println(project)
+
+  }
