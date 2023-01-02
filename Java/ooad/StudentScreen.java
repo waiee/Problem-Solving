92e61218fc22a1457f2f8295a766ed27f15cbd21
waiee <waiee_z@yahoo.com>
2023-01-03 07:32:32 +0800

    add panel

--- Java/ooad/StudentScreen.java
+++ Java/ooad/StudentScreen.java
@@ -16,6 +16,8 @@ public class StudentScreen {
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
