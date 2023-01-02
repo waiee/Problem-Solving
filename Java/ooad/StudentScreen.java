2b167a1e7dd14ebba2ffbd5b15fc8b767ee67bad
waiee <waiee_z@yahoo.com>
2023-01-03 07:26:46 +0800

    border

--- Java/ooad/StudentScreen.java
+++ Java/ooad/StudentScreen.java
@@ -29,6 +29,9 @@ public class StudentScreen {
 
         JFrame frame = new JFrame();
         
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        
     }
 
     public static void main(String[] args) {
