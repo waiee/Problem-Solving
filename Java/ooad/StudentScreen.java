d970efbe17141e3886db2f1137af5582b12e17da
waiee <waiee_z@yahoo.com>
2023-01-03 07:29:38 +0800

    button

--- Java/ooad/StudentScreen.java
+++ Java/ooad/StudentScreen.java
@@ -5,29 +5,11 @@ import java.awt.*;
 public class StudentScreen {
 
     public StudentScreen(){
-        //  // Create a new JFrame container
-        //  JFrame frame = new JFrame("Student Screen");
-        //  frame.setSize(400, 300);
- 
-        //  // Create a new JPanel
-        //  JPanel panel = new JPanel();
-        //  panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
- 
-        //  // Add a JLabel to the panel
-        //  JLabel label = new JLabel("Welcome to the Student Screen!");
-        //  panel.add(label);
- 
-        //  // Add a "View Subject" button to the panel
-        //  JButton viewSubjectButton = new JButton("View Subject");
-        //  panel.add(viewSubjectButton);
- 
-        //  // Add the panel to the frame
-        //  frame.add(panel, BorderLayout.CENTER);
-        //  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //  frame.pack();
-        //  frame.setVisible(true);
 
         JFrame frame = new JFrame();
+
+        JButton button = new JButton();
+        
         
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
