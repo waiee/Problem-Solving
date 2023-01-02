9a89a76452fb2d2f986a38799e475e2e6a3e419d
waiee <waiee_z@yahoo.com>
2023-01-03 07:28:50 +0800

    button

--- Java/ooad/StudentScreen.java
+++ Java/ooad/StudentScreen.java
@@ -31,7 +31,13 @@ public class StudentScreen {
         
         JPanel panel = new JPanel();
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        
+        panel.setLayout(new GridLayout(0, 1));
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Welcomt to Student Screen!");
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
